9c44ca123f5c89590f8f41636652826c8dbe28f4
lionmiya <lionmiya1016@gmail.com>
2018-01-20 23:03:30 +0900

    Update acollect.php

--- acollect.php
+++ acollect.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 include('conn.php');
 session_start();
 $uID=$_SESSION['uID'];
@@ -7,9 +7,9 @@ $result=mysql_query("SELECT * FROM attachment WHERE aID=$aID");
 $row=mysql_fetch_array($result);
 $check_query=mysql_query("SELECT * FROM acollect WHERE aID='$aID' AND uID='$uID' LIMIT 1");
 if(mysql_fetch_array($check_query)){
-	echo "<script>alert('已收藏');location.href='http://localhost:8080/graduationproject/acontext.php?aID=$aID'</script>";
+	echo "<script>alert('已收藏');location.href=''</script>";
 	exit;
 }
 $result2=mysql_query("INSERT INTO acollect(uID,aID,title)VALUES('$uID','$aID','$row[1]')");
-header("location:http://localhost:8080/graduationproject/acontext.php?aID=$aID");
+header("");
 ?>
