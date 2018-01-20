5d0e5a5802fb4ede302c0909fe12e6cc2aada903
lionmiya <lionmiya1016@gmail.com>
2018-01-20 22:56:00 +0900

    Update LoginSave.php

--- LoginSave.php
+++ LoginSave.php
@@ -1,22 +1,22 @@
 <?php
 session_start();
 include('conn.php');
-$username = ($_POST['username']);
-$password = MD5($_POST['password']);
-$imgcode = ($_POST['imgCode']);
-$imagecode  = ($_SESSION['thisimagecode']) ;
+$username=($_POST['username']);
+$password=MD5($_POST['password']);
+$imgcode=($_POST['imgCode']);
+$imagecode=($_SESSION['thisimagecode']) ;
 
 if($imgcode!=$imagecode){
      echo '<center>请输入正确的验证码<a href="javascript:history.back(-1);">返回</a></center>';
-	 exit;
+     exit;
 }
 //验证登录
-$check_query = mysql_query("SELECT uID FROM user WHERE username='$username' AND password='$password' limit 1");
-if($result = mysql_fetch_array($check_query)){
+$check_query=mysql_query("SELECT uID FROM user WHERE username='$username' AND password='$password' limit 1");
+if($result=mysql_fetch_array($check_query)){
 	//登录成功
-	$_SESSION['uID'] = $result['uID'];
-	$_SESSION['username'] = $username;
-    $_SESSION['password'] = $password;
+	$_SESSION['uID']=$result['uID'];
+	$_SESSION['username']=$username;
+        $_SESSION['password']=$password;
 	if($_SESSION['username']=="admin"){
 		echo '<center>欢迎进入<a href="manage.php" target="_top">管理页面</a><br/></center>';
 	}else{
