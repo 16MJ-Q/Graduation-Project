b3ae8f7fdad0846ba20ea266091cf0ffa8db5623
lionmiya <lionmiya1016@gmail.com>
2018-01-20 23:02:37 +0900

    Update acancel.php

--- acancel.php
+++ acancel.php
@@ -4,5 +4,5 @@ session_start();
 $uID=$_SESSION['uID'];
 $aID=$_GET['aID'];
 $result=mysql_query("DELETE FROM acollect WHERE uID='$uID' AND aID='$aID'");
-header("location:http://localhost:8080/graduationproject/my.php");
+header("");
 ?>
